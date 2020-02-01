2402711fea0612818f5e216cc89e3df5de33b2d5
reyzaldy <Reyzaldy Indra>
2020-02-01 16:12:54 +0700

    debug ner

--- blackboard/views.py
+++ blackboard/views.py
@@ -22,24 +22,24 @@ class ListenerAPI(APIView):
         #baca data post request
         post_data = request.data
         sentence = post_data['sentence']
-        
-        respon = requests.get('http://111.223.254.14/nlp/?sentence_intent='+ sentence)
-        # respon_ner = requests.get('http://111.223.254.14/ner/?sentence_ner=' + sentence)
-        response_data = respon.json()
-        # response_data_ner = respon_ner.json()
-        ans = response_data['ans']
-        # ans_ner = response_data_ner['ner']
-        print(ans)
-        # print(ans_ner)
+        # Berapa biaya BPJS untuk kelas 1?
+        # respon = requests.get('http://111.223.254.14/nlp/?sentence_intent='+ sentence)
+        respon_ner = requests.get('http://111.223.254.14/ner/?sentence_ner=' + sentence)
+        # response_data = respon.json()
+        response_data_ner = respon_ner.json()
+        # ans = response_data['ans']
+        ans_ner = response_data_ner['ner']
+        # print(ans)
+        print(ans_ner)
 
         #proses masukin sini
         #ambil record di DB berdasarkan hasil proses
         
-        # string_ner = ''.join(ans_ner)
+        string_ner = ''.join(ans_ner)
         # print(string_ner)
             
-        answer = DATASET_ANSWER.objects.get(answer_intent=ans)
-        # answer = DATASET_ANSWER.objects.get(answer_ner=string_ner)
+        # answer = DATASET_ANSWER.objects.get(answer_intent=ans)
+        answer = DATASET_ANSWER.objects.get(answer_ner=string_ner)
         # .get(answer_ner=string_ner)
         # print(ans_ner)
 
