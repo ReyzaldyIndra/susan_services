e35ada3a0dbc7a2fd96c281489cf615910e03bd6
reyzaldy <Reyzaldy Indra>
2020-03-07 18:54:24 +0700

    update API detectIntent and registerUser

--- blackboard/views.py
+++ blackboard/views.py
@@ -28,42 +28,68 @@ class AnswerViewSet(viewsets.ModelViewSet):
 class ListenerAPI(APIView):
     def post(self, request):
         cursor = db.cursor()
-        
+        cursor_ktp = db.cursor()
+        cursor_user = db.cursor()
+        string_ans = "userIdNULL"
+        ans="User is not registered yet"
     
         # if db.is_connected():
         #     print("Successfully connected to the DB")
         #baca data post request
         # ktp = ""
-        
         post_data = request.data
         sentence = post_data['sentence']
+        userLineId = post_data['userLineId']
+        q_user = "SELECT id_user_line,no_ktp FROM tbl_user WHERE id_user_line='" +userLineId+"';"
+        cursor_user.execute(q_user)
+        result_user =  cursor_user.fetchall()
+        for id_data in result_user:
+            print(id_data[0])
+            print(id_data[1])
+            # db_user_line_id = id_data
+            if (id_data[0] == ""):
+                string_ans = "userIdNULL"
+                ans="User is not registered yet"
+                
+            elif (id_data[0] != "" and id_data[1] == ""):
+                # q_ktp = "SELECT no_ktp FROM tbl_user WHERE id_user_line='" +userLineId+"';"
+                # cursor_ktp.execute(q_ktp)
+                # result_ktp = cursor_ktp.fetchall()
+                # for ktp_data in result_ktp:
+                #     db_ktp = ktp_data
+                # if (db_ktp == ""):
+                string_ans = "ktpNULL"
+                ans="No user KTP registered yet"
+                # elif (db_ktp != ""):
+            elif (id_data[0] != "" and id_data[1] != ""):
+
         # Berapa biaya BPJS untuk kelas 1?
-        respon = requests.get('http://111.223.254.14:5000/?sentence_intent='+ sentence)
+                respon = requests.get('http://111.223.254.14:5000/?sentence_intent='+ sentence)
         # respon_ner = requests.get('http://111.223.254.14:5001/?sentence_ner=' + sentence, verify=False, timeout=100)
-        response_data = respon.json()
+                response_data = respon.json()
         # response_data_ner = respon_ner.json()
-        ans = response_data['ans']
+                ans = response_data['ans']
         # ans_ner = response_data_ner['ner']
-        print(ans)
-        if (ans == "TRANSACTION"):
-            q_ans = "SELECT tbl_answer.jawaban, tbl_transaction_biaya.biaya_kelas_biaya, tbl_user.nama FROM tbl_answer JOIN tbl_transaction_biaya JOIN tbl_user  ON tbl_user.id_transaction_biaya=tbl_transaction_biaya.id_transaction_biaya WHERE tbl_answer.ner='B-FIN' AND tbl_user.id_user = 1 AND tbl_answer.id_answer = 25;"
-        elif (ans == "PROFIL"):
-            q_ans = "SELECT tbl_answer.jawaban, tbl_profil.provinsi_faskes, tbl_user.nama FROM tbl_answer JOIN tbl_profil JOIN tbl_user ON tbl_user.id_profil=tbl_profil.id_profil WHERE tbl_answer.ner='B-PROVINSI_FASKES' AND tbl_user.id_user = 1 AND tbl_answer.id_answer = 2;"
-        elif (ans == 'RECORD'):
-            q_ans = "SELECT tbl_answer.jawaban, tbl_record.segmen, tbl_user.nama FROM tbl_answer JOIN tbl_record JOIN tbl_user ON tbl_user.id_profil=tbl_record.id_record WHERE tbl_answer.ner='B-SEGMEN' AND tbl_user.id_user = 1 AND tbl_answer.id_answer = 13;"
-        elif (ans == 'CLOSINGS'):
-            q_ans = "SELECT jawaban FROM tbl_answer WHERE intent='CLOSINGS';"
+                    # print(ans)
+                if (ans == "TRANSACTION"):
+                    q_ans = "SELECT tbl_answer.jawaban, tbl_transaction_biaya.biaya_kelas_biaya, tbl_user.nama FROM tbl_answer JOIN tbl_transaction_biaya JOIN tbl_user  ON tbl_user.id_transaction_biaya=tbl_transaction_biaya.id_transaction_biaya WHERE tbl_answer.ner='B-FIN' AND tbl_user.id_user = 1 AND tbl_answer.id_answer = 25;"
+                elif (ans == "PROFIL"):
+                    q_ans = "SELECT tbl_answer.jawaban, tbl_profil.provinsi_faskes, tbl_user.nama FROM tbl_answer JOIN tbl_profil JOIN tbl_user ON tbl_user.id_profil=tbl_profil.id_profil WHERE tbl_answer.ner='B-PROVINSI_FASKES' AND tbl_user.id_user = 1 AND tbl_answer.id_answer = 2;"
+                elif (ans == 'RECORD'):
+                    q_ans = "SELECT tbl_answer.jawaban, tbl_record.segmen, tbl_user.nama FROM tbl_answer JOIN tbl_record JOIN tbl_user ON tbl_user.id_profil=tbl_record.id_record WHERE tbl_answer.ner='B-SEGMEN' AND tbl_user.id_user = 1 AND tbl_answer.id_answer = 13;"
+                elif (ans == 'CLOSINGS'):
+                    q_ans = "SELECT jawaban FROM tbl_answer WHERE intent='CLOSINGS';"
         # q_info = "'SELECT biaya_kelas FROM tbl_transaction WHERE kelas_rawat='Kelas 1'"
-        cursor.execute(q_ans)
-        results = cursor.fetchall()
+                cursor.execute(q_ans)
+                results = cursor.fetchall()
         # if(ktp == ""):
         #     string_ans = "Berapa nomor KTP anda?"
         #     ktp = post_data['sentence']
         # elif(ktp != ""):
-        for data in results:
-            print(data)
-            string_ans = ','.join(data).replace(',', ' ')
-            print(string_ans)
+                for data in results:
+                        # print(data)
+                    string_ans = ','.join(data).replace(',', ' ')
+                        # print(string_ans)
             # print(ktp)
         # print(response_data_ner)
 
@@ -136,8 +162,8 @@ class PostKTPApi(APIView):
         cursor = db.cursor()
         userLineId = request.data['userLineId']
         no_ktp = request.data['ktp']
-        name = request.data['name']
-        q_register = "INSERT INTO tbl_user (no_ktp, nama, id_user_line, id_profil, id_record, id_transaction_biaya, id_transaction_iuran, id_transaction_tagihan, id_answer) VALUES ("+ no_ktp+", '"+ name +"', '" + userLineId + "', 1, 1, 1, 1, 1, NULL);"
+        # name = request.data['name']
+        q_register = "INSERT INTO tbl_user (no_ktp, nama, id_user_line, id_profil, id_record, id_transaction_biaya, id_transaction_iuran, id_transaction_tagihan, id_answer) VALUES ("+ no_ktp+", 'NULL', '" + userLineId + "', 1, 1, 1, 1, 1, NULL);"
         cursor.execute(q_register)
         db.commit()
         print(cursor.rowcount, "record(s) affected")
