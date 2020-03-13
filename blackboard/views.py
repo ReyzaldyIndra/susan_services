73940d7694bfd028e2acdf18cbbfab0f1625ea31
reyzaldy <Reyzaldy Indra>
2020-03-13 20:00:39 +0700

    revert intent detection

--- blackboard/views.py
+++ blackboard/views.py
@@ -30,81 +30,37 @@ class ListenerAPI(APIView):
         cursor = db.cursor()
         cursor_ktp = db.cursor()
         cursor_user = db.cursor()
-        string_ans = "userIdNULL"
-        ans="User is not registered yet"
+
     
-        # if db.is_connected():
-        #     print("Successfully connected to the DB")
-        #baca data post request
-        # ktp = ""
         post_data = request.data
         sentence = post_data['sentence']
         userLineId = post_data['userLineId']
-        q_user = "SELECT id_user_line,no_ktp FROM tbl_user WHERE id_user_line='" +userLineId+"';"
-        cursor_user.execute(q_user)
-        result_user =  cursor_user.fetchall()
-        for id_data in result_user:
-            print(id_data[0])
-            print(id_data[1])
-            # db_user_line_id = id_data
-            if (id_data[0] == ""):
-                string_ans = "userIdNULL"
-                ans="User is not registered yet"
-                
-            elif (id_data[0] != "" and id_data[1] == ""):
-
-                string_ans = "ktpNULL"
-                ans="No user KTP registered yet"
-                
-            elif (id_data[0] != "" and id_data[1] != ""):
 
         # Berapa biaya BPJS untuk kelas 1?
-                respon = requests.get('http://111.223.254.14:5000/?sentence_intent='+ sentence)
+        respon = requests.get('http://111.223.254.14:5000/?sentence_intent='+ sentence)
         # respon_ner = requests.get('http://111.223.254.14:5001/?sentence_ner=' + sentence, verify=False, timeout=100)
-                response_data = respon.json()
+        response_data = respon.json()
         # response_data_ner = respon_ner.json()
-                ans = response_data['ans']
+        ans = response_data['ans']
         # ans_ner = response_data_ner['ner']
                     # print(ans)
-                if (ans == "TRANSACTION"):
-                    q_ans = "SELECT tbl_answer.jawaban, tbl_transaction_biaya.biaya_kelas_biaya FROM tbl_answer JOIN tbl_transaction_biaya JOIN tbl_user  ON tbl_user.id_transaction_biaya=tbl_transaction_biaya.id_transaction_biaya WHERE tbl_answer.ner='B-FIN' AND tbl_user.no_ktp ="+ id_data[1]+" AND tbl_answer.id_answer = 25;"
-                elif (ans == "PROFIL"):
-                    q_ans = "SELECT tbl_answer.jawaban, tbl_profil.provinsi_faskes FROM tbl_answer JOIN tbl_profil JOIN tbl_user ON tbl_user.id_profil=tbl_profil.id_profil WHERE tbl_answer.ner='B-PROVINSI_FASKES' AND tbl_user.no_ktp ="+id_data[1]+" AND tbl_answer.id_answer = 2;"
-                elif (ans == 'RECORD'):
-                    q_ans = "SELECT tbl_answer.jawaban, tbl_record.segmen FROM tbl_answer JOIN tbl_record JOIN tbl_user ON tbl_user.id_profil=tbl_record.id_record WHERE tbl_answer.ner='B-SEGMEN' AND tbl_user.no_ktp ="+id_data[1]+" AND tbl_answer.id_answer = 13;"
-                elif (ans == 'CLOSINGS'):
-                    q_ans = "SELECT jawaban FROM tbl_answer WHERE intent='CLOSINGS';"
-        # q_info = "'SELECT biaya_kelas FROM tbl_transaction WHERE kelas_rawat='Kelas 1'"
-                print(q_ans)
-                cursor.execute(q_ans)
-                results = cursor.fetchall()
-        # if(ktp == ""):
-        #     string_ans = "Berapa nomor KTP anda?"
-        #     ktp = post_data['sentence']
-        # elif(ktp != ""):
-                for data in results:
-                        # print(data)
-                    string_ans = ','.join(data).replace(',', ' ')
-                        # print(string_ans)
-            # print(ktp)
-        # print(response_data_ner)
-
-        #proses masukin sini
-        #ambil record di DB berdasarkan hasil proses
+        if (ans == "TRANSACTION"):
+            q_ans = "SELECT tbl_answer.jawaban, tbl_transaction_biaya.biaya_kelas_biaya FROM tbl_answer JOIN tbl_transaction_biaya JOIN tbl_user  ON tbl_user.id_transaction_biaya=tbl_transaction_biaya.id_transaction_biaya WHERE tbl_answer.ner='B-FIN' AND tbl_user.no_ktp ="+ id_data[1]+" AND tbl_answer.id_answer = 25;"                
+        elif (ans == "PROFIL"):
+            q_ans = "SELECT tbl_answer.jawaban, tbl_profil.provinsi_faskes FROM tbl_answer JOIN tbl_profil JOIN tbl_user ON tbl_user.id_profil=tbl_profil.id_profil WHERE tbl_answer.ner='B-PROVINSI_FASKES' AND tbl_user.no_ktp ="+id_data[1]+" AND tbl_answer.id_answer = 2;"
+        elif (ans == 'RECORD'):
+            q_ans = "SELECT tbl_answer.jawaban, tbl_record.segmen FROM tbl_answer JOIN tbl_record JOIN tbl_user ON tbl_user.id_profil=tbl_record.id_record WHERE tbl_answer.ner='B-SEGMEN' AND tbl_user.no_ktp ="+id_data[1]+" AND tbl_answer.id_answer = 13;"
+        elif (ans == 'CLOSINGS'):
+            q_ans = "SELECT jawaban FROM tbl_answer WHERE intent='CLOSINGS';"
         
-        # string_ner = ','.join(ans_ner)
-        # print(string_ner)
-        # string_intent = ans
-        # answer = DATASET_ANSWER.objects.get(answer_intent=ans)
-        # answer = DATASET_ANSWER.objects.get(answer_ner=string_ner).select_related()
-        # answer = DATASET_ANSWER.objects.filter(answer_intent=ans).get(answer_ner=string_ner)
-        # answer = DATASET_ANSWER.objects.filter(answer_ner=string_ner).select_related()
-        # string_answer = ans + ' -> ' + string_ner
-        # answer = string_answer
-        # print(ans_ner)
-        # print(answer)
+        print(q_ans)
+        cursor.execute(q_ans)
+        results = cursor.fetchall()
 
-        # response
+        for data in results:
+            string_ans = ','.join(data).replace(',', ' ')
+  
+
         return Response({
             'answer': string_ans,
             'intent': ans
